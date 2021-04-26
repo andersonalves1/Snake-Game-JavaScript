a58add9c0f1dbf91d30287293364ab25e761907c
anderson <im.andersonalves@gmail.com>
2021-04-26 00:35:49 -0300

    update4
    snake food

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,6 +23,11 @@ function criarCobrinha(){
         context.fillStyle ="green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -36,9 +45,10 @@ function iniciarJogo(){
     if(snake[0].x <0 * box && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y >15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y <0 * box && direction == "up") snake[0].y = 16 * box;
-    
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
